78b61cc87094d43622a6ca7028fc34e31d80e817
vkdemoapp <31950562+vkdemoapp@users.noreply.github.com>
2020-04-13 00:02:39 +0600

    Add files via upload

---
+++ precache-manifest.0e8966bf780a5d031402bcfdbc3bda72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c09520267cccce3dcaa7eeb733c8cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca4b2283ac9a96c5b6ff",
+    "url": "/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "03fb9d0f7b012250152e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ca4b2283ac9a96c5b6ff",
+    "url": "/static/js/2.00eceb82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eceb82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fb9d0f7b012250152e",
+    "url": "/static/js/main.e07e3168.chunk.js"
+  },
+  {
+    "revision": "f055f63317eab8ed5671",
+    "url": "/static/js/runtime-main.5d55cc79.js"
+  }
+]);
